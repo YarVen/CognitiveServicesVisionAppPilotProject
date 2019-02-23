a4bac1f0fb501424f87c143af06be0e4a3b009d2
Yaroslav Venzhyk <yaroslav.venzhyk@wikrgroup.com>
2019-02-24 00:33:23 +0200

    *

--- CognitiveServicesVisionAppPilotProject/Startup.cs
+++ CognitiveServicesVisionAppPilotProject/Startup.cs
@@ -1,11 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
 
 namespace CognitiveServicesVisionAppPilotProject
 {
@@ -20,15 +20,82 @@ namespace CognitiveServicesVisionAppPilotProject
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            var resultDescribe = DescribeImage(@"C:\Users\Yaroslav\Desktop\check.jpg");
+            var resultText = ExtractText(@"C:\Users\Yaroslav\Desktop\another-check.jpg");
+
+            var resultDescribeResult = resultDescribe.Result;
+            var resultTextResult = resultText.Result;
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync(resultDescribe.Result + "\r\n\r\n\r\n" + resultText.Result);
             });
+        }
+
+        public static async Task<string> DescribeImage(string imageFilePath)
+        {
+            using (HttpClient hc = new HttpClient())
+            {
+                hc.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", "4f510468136d47d183b301dafc2bd1b5");
+
+                using (MultipartFormDataContent reqContent = new MultipartFormDataContent())
+                {
+                    var queryString = HttpUtility.ParseQueryString(string.Empty);
+                    queryString["maxCandidates"] = "1";
+                    var uri = "https://centralus.api.cognitive.microsoft.com/vision/v1.0/describe?" + queryString;
+
+                    try
+                    {
+                        var imgContent = new ByteArrayContent(System.IO.File.ReadAllBytes(imageFilePath));
+                        reqContent.Add(imgContent);
+
+                        HttpResponseMessage resp = await hc.PostAsync(uri, reqContent);
+                        string respJson = await resp.Content.ReadAsStringAsync();
+                        return respJson;
+                    }
+                    catch (System.IO.FileNotFoundException ex)
+                    {
+                        return "Invalid file path is used";
+                    }
+                    catch (AggregateException ex)
+                    {
+                        return "HTTP-request object is generated incorrectly";
+                    }
+                }
+            }
+        }
+
+        public static async Task<string> ExtractText(string imageFilePath)
+        {
+            using (HttpClient hc = new HttpClient())
+            {
+                hc.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", "4f510468136d47d183b301dafc2bd1b5");
+
+                using (MultipartFormDataContent reqContent = new MultipartFormDataContent())
+                {
+                    var queryString = HttpUtility.ParseQueryString(string.Empty);
+                    queryString["maxCandidates"] = "1";
+                    var uri = "https://centralus.api.cognitive.microsoft.com/vision/v1.0/ocr";
+
+                    try
+                    {
+                        var imgContent = new ByteArrayContent(System.IO.File.ReadAllBytes(imageFilePath));
+                        reqContent.Add(imgContent);
+
+                        HttpResponseMessage resp = await hc.PostAsync(uri, reqContent);
+                        string respJson = await resp.Content.ReadAsStringAsync();
+                        return respJson;
+                    }
+                    catch (System.IO.FileNotFoundException ex)
+                    {
+                        return "Invalid file path is used";
+                    }
+                    catch (AggregateException ex)
+                    {
+                        return "HTTP-request object is generated incorrectly";
+                    }
+                }
+            }
         }
     }
 }
